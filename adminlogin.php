8ba83884bebc73211e561110aae44ffb1b7901cc
adityakotambeti <80984795+adityakotambeti@users.noreply.github.com>
2021-09-02 16:00:06 -0400

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -1,1 +1,25 @@
+<?php
+session_start();
 
+include 'config.php';
+
+$uname = $_POST['uname'];
+$pass = $_POST['pass'];
+
+
+$sql = "SELECT email,pass FROM admin where email='$uname' and pass='$pass' ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+   // fetch data
+   $id = mysqli_fetch_array($result);
+  $_SESSION["email"] = $uname;
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+  header('Location:adminhome.php');
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    header('Location:index.php');
+  }
+$conn->close();
+?>
