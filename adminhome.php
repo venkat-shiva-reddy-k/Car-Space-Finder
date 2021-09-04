e3745e12d60c213414428491262bc856edb9e28c
bharath2206 <68519683+bharath2206@users.noreply.github.com>
2021-09-04 14:09:09 -0400

    Update adminhome.php

--- adminhome.php
+++ adminhome.php
@@ -53,7 +53,49 @@
 
         <div class="row">
 
-          <p> Under Construction</p>
+         <button type="button" class="btn btn-success" data-toggle="modal" data-target="#addcity">+ Area</button>
+
+
+
+            <!-- Modal -->
+            <div class="modal fade" id="addcity" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title" id="exampleModalLabel">Add Area</h3>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+
+                            <form method="post" action="addarea.php">
+                                <div class="form-group">
+                                    <label>Enter State</label>
+                                    <input type="text" class="form-control" name="astate" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter City</label>
+                                    <input type="text" class="form-control" name="acity" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter Area</label>
+                                    <input type="text" class="form-control" name="aplace" required>
+                                </div>
+                                <button class="btn btn-success form-control">Add Area</button>
+                            </form>
+
+
+                        </div>
+                        <!-- <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
 
         </div>
 
