91fc22be27d06a3de3c0b205bcb77c382c03437f
Jagdeep123405 <66180558+Jagdeep123405@users.noreply.github.com>
2021-09-05 14:45:33 -0400

    Update slots.php

--- slots.php
+++ slots.php
@@ -54,7 +54,78 @@
     <div class="container">
 
         <div class="row">
-           <p> Under Construction</p>
+           
+                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#addslot">+ Parking Slot</button>
+
+
+            <!-- Modal -->
+            <div class="modal fade" id="addslot" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title" id="exampleModalLabel">Add Area</h3>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+
+                            <form method="post" action="addparking.php">
+                                <div class="form-group">
+                                    <label>Select Area </label>
+                                    <select class="form-control" name="parea" required>
+                                        <?php
+                                        include'config.php';
+
+                                        $sql = "SELECT * FROM area";
+                                        $result = $conn->query($sql);
+                        
+                                        if ($result->num_rows > 0) {
+                                            // output data of each row
+                                            while ($row = $result->fetch_assoc()) {
+                                                echo '
+                                            
+                                                <option value="' . $row["aid"] . '">' . $row["area"] . '</option>
+                        
+                                        
+                                            ';
+                                            }
+                                        } else {
+                                            echo "No Data Found";
+                                        }
+                                        $conn->close();
+
+
+                                        ?>
+
+                                        <!-- 
+                                        <option value="2">Labbipet</option>
+                                        <option value="4">Krishna Lanka</option> -->
+                                    </select>
+
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter Parking Slot</label>
+                                    <input type="text" class="form-control" name="pslot" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter Price</label>
+                                    <input type="text" class="form-control" name="pprice" required>
+                                </div>
+                                <button class="btn btn-success form-control">Add Area</button>
+                            </form>
+
+
+                        </div>
+                        <!-- <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
 
         </div>
 
