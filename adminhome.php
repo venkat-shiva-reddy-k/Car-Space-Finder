fa9d9390b96e5aa08e09a4b383903b79ffbf69c5
Harsh Sharma <52374089+29-Harsh@users.noreply.github.com>
2021-09-05 23:32:51 -0400

    Update adminhome.php

--- adminhome.php
+++ adminhome.php
@@ -96,8 +96,103 @@
                     </div>
                 </div>
             </div>
+            <table class="table table-stripped table-bordered">
+                <tr style="background-color:#337ab7;color:white">
+                    <td>Area Id</td>
+                    <td>State</td>
+                    <td>City</td>
+                    <td>Area</td>
+                    <td>Action</td>
+                </tr>
+
+
+                <?php
+                include 'config.php';
+
+
+                $sql = "SELECT * FROM area";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo '
+                    
+                    <tr>
+                    <td>' . $row["aid"] . '   </td>
+                    <td>' . $row["state"] . '   </td>
+                    <td>' . $row["city"] . '   </td>
+                    <td>' . $row["area"] . '   </td>
+                    <td>   
+                      <span data-toggle="modal" data-target="#' . $row["aid"] . '" class="glyphicon glyphicon-edit"></span>  
+                   <a href="deletearea.php?id= ' . $row["aid"] . '"> <span class="glyphicon glyphicon-trash"></span> </a>
+                    </td>
+                   
+                    </tr>
+
+
+                    <div class="modal fade" id="' . $row["aid"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h3 class="modal-title" id="exampleModalLabel">Add Area</h3>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+    
+                                <form method="post" action="editarea.php">
+
+                                <input type="text" class="form-control" name="aid" value="' . $row["aid"] . '" hidden>
+
+
+                                    <div class="form-group">
+                                        <label>Enter State</label>
+                                        <input type="text" class="form-control" name="estate" value="' . $row["state"] . '" required>
+                                    </div>
+    
+                                    <div class="form-group">
+                                        <label>Enter City</label>
+                                        <input type="text" class="form-control" name="ecity" value="' . $row["city"] . '"  required>
+                                    </div>
+    
+                                    <div class="form-group">
+                                        <label>Enter Area</label>
+                                        <input type="text" class="form-control" name="earea" value="' . $row["area"] . '"  required>
+                                    </div>
+                                    <button class="btn btn-success form-control">Add Area</button>
+                                </form>
+    
+    
+                            </div>
+                            <!-- <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+
+                
+                    ';
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $conn->close();
+
+
+
+
+                ?>
+
+
+
+            </table>
 
         </div>
+        
 
 
 
