8ac2825fd681f070a5a115c62fa5cf7bbf0ae09e
venkat-shiva-reddy-k <74202904+venkat-shiva-reddy-k@users.noreply.github.com>
2021-09-09 04:49:33 -0400

    Update editslot.php

--- editslot.php
+++ editslot.php
@@ -1,1 +1,21 @@
+<?php
 
+include 'config.php';
+
+$pid = $_POST['pid'];
+$parkslot = $_POST['parkslot'];
+$status = $_POST['status'];
+$price = $_POST['price'];
+
+
+$sql = " UPDATE parkingslots SET parkslot='$parkslot', status='$status', price='$price' WHERE pid='$pid' ";
+
+if ($conn->query($sql) === TRUE) {
+    //    echo "New record created successfully";
+   // echo "Error: " . $sql . "<br>" . $conn->error;
+     header('Location:slots.php');
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
