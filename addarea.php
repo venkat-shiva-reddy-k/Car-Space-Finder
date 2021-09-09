ee0b2968524cae83536ff5f65354c06dc6e3caf3
bharath2206 <68519683+bharath2206@users.noreply.github.com>
2021-09-09 04:22:20 -0400

    Update addarea.php

--- addarea.php
+++ addarea.php
@@ -1,1 +1,22 @@
+<?php
+
+include 'config.php';
+
+$astate = $_POST['astate'];
+$acity = $_POST['acity'];
+
+$aplace = $_POST['aplace'];
+
+
+$sql = " INSERT INTO area (state, city, area)
+VALUES ('$astate', '$acity', '$aplace')";
+
+if ($conn->query($sql) === TRUE) {
+  // echo "New record created successfully";
+  header('Location:adminhome.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 
