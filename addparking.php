cbb6c52ed757a3b9ea3a559edba7900bea3fcf01
bharath2206 <68519683+bharath2206@users.noreply.github.com>
2021-09-09 04:23:57 -0400

    Update addparking.php

--- addparking.php
+++ addparking.php
@@ -1,1 +1,22 @@
 
+<?php
+
+include 'config.php';
+
+$parea = $_POST['parea'];
+$pslot = $_POST['pslot'];
+
+$pprice = $_POST['pprice'];
+
+
+$sql = " INSERT INTO parkingslots (aid, parkslot, price, status)
+VALUES ('$parea', '$pslot', '$pprice', 'Available')";
+
+if ($conn->query($sql) === TRUE) {
+  // echo "New record created successfully";
+  header('Location:slots.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
