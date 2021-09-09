108845c43517ba80f0f66a21cb329ca03c312c1c
Jagdeep123405 <66180558+Jagdeep123405@users.noreply.github.com>
2021-09-09 04:44:34 -0400

    Update deleteparkingslot.php

--- deleteparkingslot.php
+++ deleteparkingslot.php
@@ -1,1 +1,17 @@
+<?php
+include'config.php';
+
+$id=$_GET["id"];
+
+
+$sql="DELETE FROM parkingslots WHERE pid='$id';";
+	 
+
+if($conn->query($sql)===TRUE){
+
+echo "Deleted Successfully";
+header('location:slots.php');
+} else{
+echo "Error:" .$sql."<br>".$conn->error;};
+?>
 
