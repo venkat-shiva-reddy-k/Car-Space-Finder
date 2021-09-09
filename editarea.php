1bac0dd4a60bed9a80419d0b8013a9f46deb0870
bharath2206 <68519683+bharath2206@users.noreply.github.com>
2021-09-09 04:47:37 -0400

    Update editarea.php

--- editarea.php
+++ editarea.php
@@ -1,1 +1,22 @@
+<?php
+
+include 'config.php';
+
+$aid = $_POST['aid'];
+$estate = $_POST['estate'];
+$ecity = $_POST['ecity'];
+
+$earea = $_POST['earea'];
+
+$sql = " UPDATE area SET state='$estate', city='$ecity', area='$earea' WHERE aid='$aid' ";
+
+if ($conn->query($sql) === TRUE) {
+    //    echo "New record created successfully";
+   // echo "Error: " . $sql . "<br>" . $conn->error;
+     header('Location:adminhome.php');
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 
