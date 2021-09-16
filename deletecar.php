7e8fa94982bcadf341cc69278e69edc0f00dd888
Jagdeep123405 <66180558+Jagdeep123405@users.noreply.github.com>
2021-09-16 11:20:59 -0400

    Update deletecar.php

--- deletecar.php
+++ deletecar.php
@@ -1,1 +1,14 @@
 
+<?php
+include'config.php';
+
+$id=$_GET["id"];
+
+
+$sql="DELETE FROM car WHERE cid='$id';";
+	 
+if($conn->query($sql)===TRUE){
+echo "Deleted Successfully";
+header('location:user_profile.php');
+} else{
+echo "Error:" .$sql."<br>".$conn->error;}
