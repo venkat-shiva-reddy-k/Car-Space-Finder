767befb267038dab85d977df317f2812e725cc89
Harsh Sharma <52374089+29-Harsh@users.noreply.github.com>
2021-09-16 11:55:29 -0400

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -28,6 +28,15 @@
 
   <!-- CUSTOM CSS -->
   <link href="css/style.css" rel="stylesheet">
+  
+  
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 <style>
 
@@ -80,7 +89,7 @@
 <section class="user-login section">
 	<div class="container">
 		<div class="row">
-		    	<div class="col-3"></div>
+		    	
 			<div class="col-6">
 				
 
@@ -157,7 +166,7 @@
                                                     </div>
                     
                                                     <div class="form-group">
-                                                        <label>EMail</label>
+                                                        <label>Email</label>
                                                         <input type="text" class="form-control" name="uemail" value="' . $row["email"] . '"  required>
                                                     </div>
                     
@@ -171,7 +180,7 @@
                                                         <input type="text" class="form-control" name="upass" value="' . $row["pass"] . '"  required>
                                                         
                                                     </div>
-                                                    <button class="btn btn-success form-control">Update Details</button>
+                                                    <button class="btn btn-success form-control" id="updatedetails">Update Details</button>
                                                 </form>
                     
                     
@@ -196,7 +205,158 @@
            
          
             </div>
-            	<div class="col-3"></div>
+            	<div class="col-6">
+            	    
+            	    
+            	    
+
+
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#addcity">+ Add Car</button>
+
+
+
+            <!-- Modal -->
+            <div class="modal fade" id="addcity" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title" id="exampleModalLabel">Add Car</h3>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+
+                            <form method="POST" action="addcar.php">
+                                <div class="form-group">
+                                    <label>Enter Company Name</label>
+                                    <input type="text" class="form-control" name="compnay" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter Car Number</label>
+                                    <input type="text" class="form-control" name="carno" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter Color</label>
+                                    <input type="text" class="form-control" name="color" required>
+                                </div>
+                                <button class="btn btn-success form-control" id="addC">Add</button>
+                            </form>
+
+
+                        </div>
+                        <!-- <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+
+            <table class="table table-stripped table-bordered">
+                <tr style="background-color:#337ab7;color:white">
+                    <td>Car Id</td>
+                    <td>Company Name</td>
+                    <td>Carno</td>
+                    <td>Color</td>
+                    <td>Action</td>
+                </tr>
+
+
+                <?php
+                include 'config.php';
+                // session_start();
+
+                $email = $_SESSION["email"];
+
+                $sql = "SELECT * FROM car where email='$email'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo '
+                    
+                    <tr>
+                    <td>' . $row["cid"] . '   </td>
+                    <td>' . $row["carcompany"] . '   </td>
+                    <td>' . $row["carno"] . '   </td>
+                    <td>' . $row["color"] . '   </td>
+                    <td>   
+                      <span data-toggle="modal" data-target="#' . $row["cid"] . '" class="glyphicon glyphicon-edit"></span>  
+                   <a href="deletecar.php?id= ' . $row["cid"] . '"> <span class="glyphicon glyphicon-trash"></span> </a>
+                    </td>
+                   
+                    </tr>
+
+
+                    <div class="modal fade" id="' . $row["cid"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h3 class="modal-title" id="exampleModalLabel">Edit Car</h3>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+    
+                                <form method="post" action="editcar.php">
+
+                                <input type="text" class="form-control" name="cid" value="' . $row["cid"] . '" hidden>
+
+
+                                    <div class="form-group">
+                                        <label>Enter Company</label>
+                                        <input type="text" class="form-control" name="ecarcompany" value="' . $row["carcompany"] . '" required>
+                                    </div>
+    
+                                    <div class="form-group">
+                                        <label>Enter Carno</label>
+                                        <input type="text" class="form-control" name="ecarno" value="' . $row["carno"] . '"  required>
+                                    </div>
+    
+                                    <div class="form-group">
+                                        <label>Enter Color</label>
+                                        <input type="text" class="form-control" name="ecolor" value="' . $row["color"] . '"  required>
+                                    </div>
+                                    <button class="btn btn-success form-control" id="editC">Update</button>
+                                </form>
+    
+    
+                            </div>
+                            <!-- <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+
+                
+                    ';
+                    }
+                } else {
+                    echo "No Cars Found please add car";
+                }
+                $conn->close();
+
+
+
+
+                ?>
+
+
+
+            </table>
+
+    
+
+            	    
+            	    
+            	</div>
         </div>
 	</div>
 </section>
