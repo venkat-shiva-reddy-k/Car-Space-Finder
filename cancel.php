4587e82caca67a2650ff9b7e42475cce45ad1530
Harsh Sharma <52374089+29-Harsh@users.noreply.github.com>
2021-09-16 10:47:24 -0400

    Update cancel.php

--- cancel.php
+++ cancel.php
@@ -1,1 +1,13 @@
+<?php
+include'config.php';
 
+$id=$_GET["id"];
+
+
+$sql="DELETE FROM booking WHERE bid='$id';";
+	 
+if($conn->query($sql)===TRUE){
+echo "Cancelled Successfully";
+header('location:mybookings.php');
+} else{
+echo "Error:" .$sql."<br>".$conn->error;}
